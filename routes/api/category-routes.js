243545140050a603538e40bd1115cb767307c1a1
unknown <scarletfedora@gmail.com>
2022-05-18 17:56:02 -0700

    Working on CRUD routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,14 +3,35 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const CategoryData = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(CategoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const CategoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if(!CategoryData) {
+      res.status(404).json({message: "No category that matches that id!"})
+    }
+
+    res.status(200).json(CategoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,11 +6,32 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const TagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if(!TagData) {
+      res.status(404).json({message: "No category that matches that id!"})
+    }
+
+    res.status(200).json(TagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
@@ -19,6 +40,24 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(
+    {
+      // All the fields you can update and the data attached to the request body.
+      id: req.body.id,
+      tag_name: req.body.tag_name,
+    },
+    {
+      // Gets the books based on the isbn given in the request parameters
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((updatedBook) => {
+      // Sends the updated book as a json response
+      res.json(updatedBook);
+    })
+    .catch((err) => res.json(err));
 });
 
 router.delete('/:id', (req, res) => {
